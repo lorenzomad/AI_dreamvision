9b439941eb81904905eec741e7cc4d080d26c941
lorenzo <lorenzo.maddalena1@gmail.com>
2023-01-25 22:50:03 +0100

    feat: created temporary frontend app

--- frontend.py
+++ frontend.py
@@ -2,13 +2,25 @@ import kivy
 kivy.require('2.1.0')
 
 from kivy.app import App
-from kivy.uix.label import Label
+from kivy.uix.boxlayout import BoxLayout
+import random
 
+import main
+
+
+class MyRoot(BoxLayout):
+
+    def __init__(self):
+        super(MyRoot, self).__init__()
+
+    def generate_number(self):
+
+        self.random_label.text = str(random.randint(1,10))
 
 class DreamVision(App):
 
     def build(self):
-        return Label(text="DreamVision")
+        return MyRoot()
 
 dreamvision = DreamVision()
 dreamvision.run()

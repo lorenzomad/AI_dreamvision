acd8a60773c824202883a5067dee934e7ca5b2be
lorenzo <lorenzo.maddalena1@gmail.com>
2023-01-26 20:56:29 +0100

    feat: introduced multiple screens

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from craiyon import Craiyon
 
 def generate_dream(text):
     """generates pictures of the described dream"""
+    print("image generation started!")
     generator = Craiyon()
     result = generator.generate(text)
     result.save_images()

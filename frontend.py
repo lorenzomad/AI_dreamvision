bbea97c2144b240908da40673f9ab5793aef8a1c
lorenzo <lorenzo.maddalena1@gmail.com>
2023-01-26 20:02:19 +0100

    feat: connected creation of images

--- frontend.py
+++ frontend.py
@@ -1,6 +1,7 @@
 #import kivymd
 from kivymd.app import MDApp
 from kivy.lang import Builder
+from kivy.properties import ObjectProperty
 
 #from kivy.uix.boxlayout import BoxLayout
 import random
@@ -9,13 +10,22 @@ import main
 
 
 class Main(MDApp):
+    """class that runs the dreamvision.kv file to visualize the frontend"""
+    
+    dream_text = ObjectProperty(None)
 
     def build(self):
         return Builder.load_file("dreamvision.kv")
 
-    def generate_number(self):
-        label = self.root.ids.random_label
-        label.text = str(random.randint(1,10))
+    def generate_dream(self):
+        """generates a random number"""
+        
+        if self.root.dream_text.text != None:
+            main.generate_dream(self.root.dream_text.text)
+        #label = self.root.ids.random_label
+        #label.text = str(random.randint(1,10))
+        
+    
 
 
 

456af3f79e603d0667f24d8e3f02e76d7e1b5902
lorenzo <lorenzo.maddalena1@gmail.com>
2023-01-26 21:09:06 +0100

    feat: deleted frontend

--- main.py
+++ main.py
@@ -1,10 +1,42 @@
 from craiyon import Craiyon
 
-def generate_dream(text):
-    """generates pictures of the described dream"""
-    print("image generation started!")
-    generator = Craiyon()
-    result = generator.generate(text)
-    result.save_images()
-    print("images generated")
-    return result
+
+from kivymd.app import MDApp
+from kivy.lang import Builder
+from kivy.properties import ObjectProperty
+from kivy.uix.screenmanager import Screen, ScreenManager
+from kivy.uix.boxlayout import BoxLayout
+ 
+
+#from kivy.uix.boxlayout import BoxLayout
+import random
+
+import main
+
+
+class Main(MDApp):
+    """class that runs the dreamvision.kv file to visualize the frontend"""
+    
+    dream_text = ObjectProperty(None)
+
+    def build(self):
+        return Builder.load_file("dreamvision.kv")
+
+    def generate_dream(self):
+        """generates dream images"""
+        
+        if self.root.dream_text.text != None:
+            
+            print("image generation started!")
+            generator = Craiyon()
+            result = generator.generate(self.root.dream_text.text)
+            result.save_images()
+            print("images generated")
+            return result
+        
+        
+dreamvision = Main()
+dreamvision.run()
+
+
+    

e6ef5fd9e14b1a8832de535be7158f3294dac96e
lorenzo <lorenzo.maddalena1@gmail.com>
2023-02-08 21:28:36 +0100

    changed path in test_main

--- src/dreamvision/test_main.py
+++ src/dreamvision/test_main.py
@@ -1,32 +1,36 @@
+import sys
 import pytest
 import os
 import base64
 import sqlite3
 import kivymd
+import pathlib
 
+#local imports
+sys.path.insert(0, str(pathlib.Path(__file__).parent))
 import main
 
 
-# def test_save_image():
-#     if not os.path.exists("./test_images/"):
-#         os.mkdir("./test_images/")
-#     image = "iVBORw0KGgoAAAANSUhEUgAAA4QAAAFKCAIAAADKUQaBAAAAAXNSR0IArs4c6QAAAARnQU1BAACxjwv8YQUAAAAJcEhZcwAADsMAAA7DAcdvqGQAAP+lSURBVHhepP1p32zb"
-#     image_data = base64.b64decode(image.replace(" ", "+"))
-#     main.save_image(image_data, 'test_images', 'test ImAge')
-#     assert os.path.exists('test_images/test_image.jpg') == True
-#     main.save_image(image_data, 'test_images', 'test ImAge')
-#     assert os.path.exists('test_images/test_image(1).jpg') == True
-#     os.remove('test_images/test_image.jpg')
-#     os.remove('test_images/test_image(1).jpg')
+def test_save_image():
+    if not os.path.exists("./test_images/"):
+        os.mkdir("./test_images/")
+    image = "iVBORw0KGgoAAAANSUhEUgAAA4QAAAFKCAIAAADKUQaBAAAAAXNSR0IArs4c6QAAAARnQU1BAACxjwv8YQUAAAAJcEhZcwAADsMAAA7DAcdvqGQAAP+lSURBVHhepP1p32zb"
+    image_data = base64.b64decode(image.replace(" ", "+"))
+    main.save_image(image_data, 'test_images', 'test ImAge')
+    assert os.path.exists('test_images/test_image.jpg') == True
+    main.save_image(image_data, 'test_images', 'test ImAge')
+    assert os.path.exists('test_images/test_image(1).jpg') == True
+    os.remove('test_images/test_image.jpg')
+    os.remove('test_images/test_image(1).jpg')
     
-# def test_load_last_image():
-#     connection = sqlite3.connect('dreams.db')
-#     cursor = connection.cursor()
-#     cursor.execute("""insert into dreams (title, description, date, file_name)
-#         values ('test_image','test image', 2023-01-04, 'test_images/test_image.jpg');""")
-#     assert main.load_last_image(connection) == 'test_images/test_image.jpg'
-#     cursor.execute("""insert into dreams (title, description, date, file_name)
-#         values ('test_image','test image', 2023-01-04, 'test_images/test_image2.jpg');""")
-#     assert main.load_last_image(connection) != 'test_images/test_image.jpg'
-#     cursor.execute("""delete from dreams where file_name = 'test_images/test_image.jpg'""")
+def test_load_last_image():
+    connection = sqlite3.connect('dreams.db')
+    cursor = connection.cursor()
+    cursor.execute("""insert into dreams (title, description, date, file_name)
+        values ('test_image','test image', 2023-01-04, 'test_images/test_image.jpg');""")
+    assert main.load_last_image(connection) == 'test_images/test_image.jpg'
+    cursor.execute("""insert into dreams (title, description, date, file_name)
+        values ('test_image','test image', 2023-01-04, 'test_images/test_image2.jpg');""")
+    assert main.load_last_image(connection) != 'test_images/test_image.jpg'
+    cursor.execute("""delete from dreams where file_name = 'test_images/test_image.jpg'""")
 
